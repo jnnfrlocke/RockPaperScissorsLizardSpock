ef525a5e83ced14f0aee1d1ab37a825afcedec02
Jen <jnnfrlocke@gmail.com>
2017-09-08 10:36:42 -0500

    Add Shape class and create object files

---
+++ RockPaperScissorsLizardSpock/Shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissorsLizardSpock
+{
+    public class Shape
+    {
+        string wins1;
+        string wins2;
+        string loses1;
+        string loses2;
+    }
+}
